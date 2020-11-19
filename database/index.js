7e91e9776e02d9d69e1bbd3f4f2484a7b055925c
Dhia-Fattoum <dhiafattoum@outlook.com>
2020-11-19 14:49:07 +0100

    Procfile

--- database/index.js
+++ database/index.js
@@ -3,7 +3,6 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize("b7ntfkr2a8u3hwajcm3s", "uvullsjgrdrwg4q3", "lCrNLDAxMglq1SQrckm1", {
     host:"b7ntfkr2a8u3hwajcm3s-mysql.services.clever-cloud.com",
     dialect: 'mysql',
-    port: 3306,
 });
 
 sequelize
