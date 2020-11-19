cde2bc81ca1458409019505c0c26ae2f8d110633
Dhia-Fattoum <dhiafattoum@outlook.com>
2020-11-19 17:26:45 +0100

    prePush

---
+++ routes/media.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const Media = require('../models/mediaSchema');
+
+
+router.get('/', async (req, res) => {
+    await Media.findAll().then((users) => res.json(users))
+  })
+  router.get('/:id', async (req, res) => {
+      await Media.findOne({ id: req.params.id })
+  })
+  router.post("/SignUp", async (req, res) => {
+    const urlExists = await Media.findOne({
+      where: { fileUrl: req.body.fileUrl },
+    });
+    await Users.create({
+        fileUrl:req.body.fileUrl,
+        duration:req.body.duration,
+        isVideo : req.body.isVideo,
+        postID : req.body.post
+    }).then((user) => res.json(user));
+  });
+
+
+  module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
