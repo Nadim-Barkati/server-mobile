85b59d4bb41745ddd22a60610bac1587b5cbe08b
Dhia-Fattoum <dhiafattoum@outlook.com>
2020-11-19 14:37:09 +0100

    Procfile

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USER, process.env.MYSQL_ADDON_PASSWORD, {
     host: process.env.MYSQL_ADDON_HOST,
     dialect: 'mysql',
-    
+    port: process.env.MYSQL_ADDON_PORT,
 });
 
 sequelize
