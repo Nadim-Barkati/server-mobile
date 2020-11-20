92f65c6324e31efb25baa2bebcef5acba427e80a
Dhia-Fattoum <dhiafattoum@outlook.com>
2020-11-20 10:13:01 +0100

    compleeting the schemas

--- routes/media.js
+++ routes/media.js
@@ -4,44 +4,45 @@ const Media = require('../models/mediaSchema');
 
 
 router.get('/', async (req, res) => {
-    await Media.findAll().then((users) => res.json(users))
+    await Media.findAll().then((media) => res.json(media))
   })
   router.get('/:id', async (req, res) => {
       await Media.findOne({ id: req.params.id })
   })
-  router.post("/SignUp", async (req, res) => {
-    const urlExists = await Media.findOne({
-      where: { fileUrl: req.body.fileUrl },
-    });
+  router.post("/AddMedia", async (req, res) => {
+    console.log(req.body)
     await Users.create({
         fileUrl:req.body.fileUrl,
         duration:req.body.duration,
         isVideo : req.body.isVideo,
         postID : req.body.post
-    }).then((user) => res.json(user));
-  });
-
+    })
+    .then((adress) => res.json(adress))
+    .catch((err) => console.log(err))
+  })
+router.put("/:id",async (req, res)=>{
+  Media.findByPk(req.params.id).then((media) => {
+    media
+      .update({
+        fileUrl:req.body.fileUrl,
+        duration:req.body.duration,
+        isVideo : req.body.isVideo,
+        postID : req.body.post
+      })
+      .then((media) => {
+        res.json(media);
+      });   
+    })
+})
+router.delete("/:id", async (req, res) => {
+  await Media.findByPk(req.params.id)
+    .then((media) => {
+      media.destroy();
+    })
+    .then(() => {
+      res.json("deleted");
+    });
+});
 
   module.exports = router;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = router;
