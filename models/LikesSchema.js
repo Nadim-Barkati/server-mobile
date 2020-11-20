28cc7efc4bedcc27043615bb3fb3333c143949bc
helayounsi <hela.ahmedyounsi@gmail.com>
2020-11-20 04:21:38 +0100

    addad router for likes

--- models/LikesSchema.js
+++ models/LikesSchema.js
@@ -2,7 +2,7 @@ const { Model, Sequelize } = require('sequelize');
 const sequelize = require('../database/index')
 class Likes extends Model {}
 Likes.init({
-    IDPost: Sequelize.INTEGER,
+    IdPost: Sequelize.INTEGER,
     UserId: Sequelize.INTEGER,
 
 }, { sequelize, modelName: 'likes' });
--- routes/media.js
+++ routes/media.js
@@ -4,11 +4,15 @@ const Media = require('../models/mediaSchema');
 
 
 router.get('/', async (req, res) => {
+  //it must be maybe media instead of user!
+  //can we add acondition
     await Media.findAll().then((users) => res.json(users))
   })
   router.get('/:id', async (req, res) => {
       await Media.findOne({ id: req.params.id })
   })
+
+  //you need maybe to change the route!!!!
   router.post("/SignUp", async (req, res) => {
     const urlExists = await Media.findOne({
       where: { fileUrl: req.body.fileUrl },
@@ -17,7 +21,8 @@ router.get('/', async (req, res) => {
         fileUrl:req.body.fileUrl,
         duration:req.body.duration,
         isVideo : req.body.isVideo,
-        postID : req.body.post
+        postID : req.body.post,
+        //it must be maybe media instead of user!
     }).then((user) => res.json(user));
   });
 
