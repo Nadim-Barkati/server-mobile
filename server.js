cb381159d6ec6fa3705e49e10de238a11ec8fb89
Dhia-Fattoum <dhiafattoum@outlook.com>
2020-12-01 15:57:43 +0100

    try to deploy heroku final

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express') ;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-// require('dotenv').config()
+
 const server = require("http").createServer(app);
 const PORT =process.env.PORT || 3000; 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
 
 app.use('/User', require("./routes/user.js"));
 app.use('/Post',require("./routes/post.js"));
@@ -17,7 +16,9 @@ app.use('/Like',require("./routes/like.js"));
 app.use('/Conversation',require("./routes/conversation.js"));
 app.use('/Message',require("./routes/message.js"));
 
-
+app.get('/',(req,res) =>{
+  res.send('hello')
+})
   
 
 server.listen(PORT, () => {
